9ed235d020a4ca7795d278a1ea7bea68b8ab4d3b
i1rr <rivan1986@gmail.com>
2023-01-10 12:51:28 +1100

    candidate edit, delete

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -2,8 +2,8 @@ package ru.job4j.dreamjob.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.repository.CandidateRepository;
 import ru.job4j.dreamjob.repository.MemoryCandidateRepository;
 
@@ -13,6 +13,17 @@ public class CandidateController {
 
     private final CandidateRepository candidateRepository = MemoryCandidateRepository.getInstance();
 
+    @GetMapping("/{id}")
+    public String getById(Model model, @PathVariable int id) {
+        var candidateOptional = candidateRepository.findById(id);
+        if (candidateOptional.isEmpty()) {
+            model.addAttribute("message", "Candidate with id " + id + " not found");
+            return "errors/404";
+        }
+        model.addAttribute("candidate", candidateOptional.get());
+        return "candidates/one";
+    }
+
     @GetMapping
     public String getAll(Model model) {
         model.addAttribute("candidates", candidateRepository.findAll());
@@ -20,7 +31,33 @@ public class CandidateController {
     }
 
     @GetMapping("/create")
-    public String create() {
+    public String getCreationPage() {
         return "candidates/create";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(Model model, @PathVariable int id) {
+        var isDeleted = candidateRepository.deleteById(id);
+        if (!isDeleted) {
+            model.addAttribute("message", "Candidate with id " + id + " not found");
+            return "errors/404";
+        }
+        return "redirect:/candidates";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute Candidate candidate) {
+        candidateRepository.save(candidate);
+        return "redirect:/candidates";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Candidate candidate, Model model) {
+        var isUpdated = candidateRepository.update(candidate);
+        if (!isUpdated) {
+            model.addAttribute("message", "Candidate with id " + candidate.getId() + " not found");
+            return "errors/404";
+        }
+        return "redirect:/candidates";
     }
 }
--- src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
+++ src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
@@ -60,5 +60,4 @@ public class VacancyController {
         }
         return "redirect:/vacancies";
     }
-
 }
